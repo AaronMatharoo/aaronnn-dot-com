743c6e03cbe340696d12fdd7501d6fea9f3cbf23
arizona <aaronsmatharoo@gmail.com>
2023-08-25 15:31:24 -0400

    marquee created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,42 @@
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
+  const SlidingTexts = [
+    "Music",
+    "App Development",
+    "Racing",
+    "Design",
+    "Hip-Hop",
+    "Engineering",
+    "R&B",
+    "Programming",
+    "Cyber Security",
+    "Fashion",
+    "Software Engineering",
+    "Automotive Engineering",
+    "Charity",
+  ];
 
-  const ReactTester = () => {
+  const SlidingBanner = () => {
     return (
       <>
-      <div>
-        <h1 className=' text-red-600 text-9xl'>
-          this is a react component playaaa
-        </h1>
-      </div>
+        <div className="w-full h-12 border-2 border-black overflow-hidden relative">
+          <Marquee>
+            {SlidingTexts.map((text) => (
+              <div className="mt-2 flex items-center whitespace-nowrap flex-shrink-0">
+                <p className="text-lg">{text}</p>
+                <img src="star.png" className="mx-5 h-5" />
+              </div>
+            ))}
+          </Marquee>
+        </div>
       </>
-    )
-  }
+    );
+  };
 
   return (
     <div>
-        <p>Welcome to my houseyyyyy!" </p>
-        <ReactTester />
-        <p>
-          Get started by becoming the best
-        </p>
+      <SlidingBanner />
     </div>
-  )
+  );
 }
