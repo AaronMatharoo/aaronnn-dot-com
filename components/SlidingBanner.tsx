0e43472bacf7f3e78343bfc9a16105ad23f4a14b
arizona <aaronsmatharoo@gmail.com>
2023-08-25 17:36:58 -0400

    responsive grid created

---
+++ components/SlidingBanner.tsx
@@ -0,0 +1,42 @@
+import Marquee from "react-fast-marquee";
+
+const SlidingBanner = () => {
+    const SlidingTexts = [
+        "Music",
+        "App Development",
+        "Racing",
+        "Design",
+        "Hip-Hop",
+        "Engineering",
+        "R&B",
+        "Programming",
+        "Cyber Security",
+        "Fashion",
+        "Software Engineering",
+        "Automotive Engineering",
+        "Charity",
+      ];
+    
+      const Slider = () => {
+        return (
+          <>
+            <div className="w-full py-2 h-12 border-y-2 border-black overflow-hidden relative">
+              <Marquee>
+                {SlidingTexts.map((text) => (
+                  <div className=" flex items-center whitespace-nowrap flex-shrink-0">
+                    <p className="text-lg font-light">{text}</p>
+                    <img src="aa.png" className="mx-5 h-7" />
+                  </div>
+                ))}
+              </Marquee>
+            </div>
+          </>
+        );
+      };
+
+      return (
+        <Slider />
+      )
+}
+
+export default SlidingBanner
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,27 @@
+import SlidingBanner from "@components/SlidingBanner";
 import Marquee from "react-fast-marquee";
 
 export default function Home() {
-  const SlidingTexts = [
-    "Music",
-    "App Development",
-    "Racing",
-    "Design",
-    "Hip-Hop",
-    "Engineering",
-    "R&B",
-    "Programming",
-    "Cyber Security",
-    "Fashion",
-    "Software Engineering",
-    "Automotive Engineering",
-    "Charity",
-  ];
-
-  const SlidingBanner = () => {
-    return (
-      <>
-        <div className="w-full h-12 border-2 border-black overflow-hidden relative">
-          <Marquee>
-            {SlidingTexts.map((text) => (
-              <div className="mt-2 flex items-center whitespace-nowrap flex-shrink-0">
-                <p className="text-lg">{text}</p>
-                <img src="star.png" className="mx-5 h-5" />
-              </div>
-            ))}
-          </Marquee>
-        </div>
-      </>
-    );
-  };
 
   return (
-    <div>
-      <SlidingBanner />
+    <div className="h-screen flex flex-col">
+      <div>
+        <SlidingBanner />
+      </div>
+      <div className="flex-1 bg-slate-400 grid grid-rows-6 grid-cols-6">
+        <div className=" row-span-3 col-span-4 bg-red-500 border-r-2 border-b-2 border-black hover:scale-95 duration-100 hover:border-t-2 hover:border-l-2">
+          About
+        </div>
+        <div className=" row-span-3 col-span-2 bg-purple-500 border-r-2 border-b-2 border-black hover:scale-95 duration-100 hover:border-t-2 hover:border-l-2">
+          Aboutsdsdsd
+        </div>
+        <div className=" row-span-3 col-span-2 bg-yellow-500 border-r-2 border-b-2 border-black hover:scale-95 duration-100 hover:border-t-2 hover:border-l-2">
+          dsadsd
+        </div>
+        <div className=" row-span-3 col-span-4 bg-green-500 border-r-2 border-b-2 border-black hover:scale-95 duration-100 hover:border-t-2 hover:border-l-2">
+          eqweerrewrew
+        </div>
+      </div>
     </div>
   );
 }
